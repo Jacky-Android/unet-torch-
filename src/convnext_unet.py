cd3ffab857d3d9982bf28bfd1a79071b26180b42
Jacky-Android <55181594+Jacky-Android@users.noreply.github.com>
2022-12-17 22:05:49 +0800

    Update convnext_unet.py

--- src/convnext_unet.py
+++ src/convnext_unet.py
@@ -54,29 +54,29 @@ class U_ConvNext(nn.Module):
 
         backbone = backbone.features
         
-        stage_indices = [0,1,2,4,7]
-        self.stage_out_channels = [128, 128,256,512,1024]
+        stage_indices = [1,2,4,7]
+        self.stage_out_channels = [128,256,512,1024]
         return_layers = dict([(str(j), f"stage{i}") for i, j in enumerate(stage_indices)])
         self.backbone = IntermediateLayerGetter(backbone, return_layers=return_layers)
 
-        c = self.stage_out_channels[4] + self.stage_out_channels[3]
-        self.up1 = Up(c, self.stage_out_channels[3])
+        #c = self.stage_out_channels[4] + self.stage_out_channels[3]
+        self.up1 = Up(self.stage_out_channels[3], self.stage_out_channels[2])
         c = self.stage_out_channels[3] + self.stage_out_channels[2]
-        self.up2 = Up(c, self.stage_out_channels[2])
+        self.up2 = Up(self.stage_out_channels[2], self.stage_out_channels[1])
         c = self.stage_out_channels[2] + self.stage_out_channels[1]
-        self.up3 = Up(c, self.stage_out_channels[1])
+        self.up3 = Up(self.stage_out_channels[1], self.stage_out_channels[0])
         c = self.stage_out_channels[1] + self.stage_out_channels[0]
-        self.up4 = Up(c, self.stage_out_channels[0])
+        #self.up4 = Up(self.stage_out_channels[1], self.stage_out_channels[0])
         self.conv = OutConv(self.stage_out_channels[0], num_classes=num_classes)
 
     def forward(self, x: torch.Tensor) -> Dict[str, torch.Tensor]:
         input_shape = x.shape[-2:]
         backbone_out = self.backbone(x)
         
-        x = self.up1(backbone_out['stage4'], backbone_out['stage3'])
-        x = self.up2(x, backbone_out['stage2'])
-        x = self.up3(x, backbone_out['stage1'])
-        x = self.up4(x, backbone_out['stage0'])
+        x = self.up1(backbone_out['stage3'], backbone_out['stage2'])
+        x = self.up2(x, backbone_out['stage1'])
+        x = self.up3(x, backbone_out['stage0'])
+        #x = self.up4(x, backbone_out['stage0'])
         x = self.conv(x)
         x = F.interpolate(x, size=input_shape, mode="bilinear", align_corners=False)
         return {"out": x}
