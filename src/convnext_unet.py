b9e15b17b07828d3a2d6d9217278ea856c3953c3
Jacky-Android <55181594+Jacky-Android@users.noreply.github.com>
2022-12-17 13:37:12 +0800

    Update convnext_unet.py

--- src/convnext_unet.py
+++ src/convnext_unet.py
@@ -1,217 +1,80 @@
+from collections import OrderedDict
+from typing import Dict
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
-from zmq import device
+from torch import Tensor
+from torchvision.models import convnext_base
+from .unetvgg16 import Up, OutConv
 
 
-class LayerNorm(nn.Module):
-    r"""LayerNorm that supports two data formats: channels_last (default) or channels_first.
-    The ordering of the dimensions in the inputs. channels_last corresponds to inputs with
-    shape (batch_size, height, width, channels) while channels_first corresponds to inputs
-    with shape (batch_size, channels, height, width).
-    """
+class IntermediateLayerGetter(nn.ModuleDict):
+    
+    _version = 2
+    __annotations__ = {
+        "return_layers": Dict[str, str],
+    }
 
-    def __init__(self, normalized_shape, eps=1e-6, data_format="channels_last"):
-        super().__init__()
-        self.weight = nn.Parameter(torch.ones(normalized_shape))
-        self.bias = nn.Parameter(torch.zeros(normalized_shape))
-        self.eps = eps
-        self.data_format = data_format
-        if self.data_format not in ["channels_last", "channels_first"]:
-            raise NotImplementedError
-        self.normalized_shape = (normalized_shape,)
+    def __init__(self, model: nn.Module, return_layers: Dict[str, str]) -> None:
+        if not set(return_layers).issubset([name for name, _ in model.named_children()]):
+            raise ValueError("return_layers are not present in model")
+        orig_return_layers = return_layers
+        return_layers = {str(k): str(v) for k, v in return_layers.items()}
 
-    def forward(self, x):
-        if self.data_format == "channels_last":
-            return F.layer_norm(
-                x, self.normalized_shape, self.weight, self.bias, self.eps
-            )
-        elif self.data_format == "channels_first":
-            u = x.mean(1, keepdim=True)
-            s = (x - u).pow(2).mean(1, keepdim=True)
-            x = (x - u) / torch.sqrt(s + self.eps)
-            x = self.weight[:, None, None] * x + self.bias[:, None, None]
-            return x
+        # 重新构建backbone，将没有使用到的模块全部删掉
+        layers = OrderedDict()
+        for name, module in model.named_children():
+            layers[name] = module
+            if name in return_layers:
+                del return_layers[name]
+            if not return_layers:
+                break
 
+        super(IntermediateLayerGetter, self).__init__(layers)
+        self.return_layers = orig_return_layers
 
-class ConvNectBlock(nn.Module):
-    r"""ConvNeXt ConvNectBlock. There are two equivalent implementations:
-    (1) DwConv -> LayerNorm (channels_first) -> 1x1 Conv -> GELU -> 1x1 Conv; all in (N, C, H, W)
-    (2) DwConv -> Permute to (N, H, W, C); LayerNorm (channels_last) -> Linear -> GELU -> Linear; Permute back
-    We use (2) as we find it slightly faster in PyTorch
-    Args:
-        dim (int): Number of input channels.
-        drop_path (float): Stochastic depth rate. Default: 0.0
-        layer_scale_init_value (float): Init value for Layer Scale. Default: 1e-6.
-    """
-
-    def __init__(self, dim, drop_path=0.0, layer_scale_init_value=1e-6):
-        super().__init__()
-        self.dwconv = nn.Conv2d(
-            dim, dim, kernel_size=7, padding=3, groups=dim)  # depthwise conv
-        self.norm = LayerNorm(dim, eps=1e-6)
-        self.pwconv1 = nn.Linear(
-            dim, 4 * dim
-        )  # pointwise/1x1 convs, implemented with linear layers
-        self.act = nn.GELU()
-        self.pwconv2 = nn.Linear(4 * dim, dim)
-        self.gamma = (
-            nn.Parameter(layer_scale_init_value * torch.ones((dim)), requires_grad=True)
-            if layer_scale_init_value > 0
-            else None
-        )
-        self.drop_path = nn.Identity()
-
-    def forward(self, x):
-        input = x
-        x = self.dwconv(x)
-        x = x.permute(0, 2, 3, 1)  # (N, C, H, W) -> (N, H, W, C)
-        x = self.norm(x)
-        x = self.pwconv1(x)
-        x = self.act(x)
-        x = self.pwconv2(x)
-        if self.gamma is not None:
-            x = self.gamma * x
-        x = x.permute(0, 3, 1, 2)  # (N, H, W, C) -> (N, C, H, W)
-        x = input + self.drop_path(x)
-        return x
-
-
-class UpConvNext(nn.Module):
-    def __init__(self, ch_in, ch_out):
-        super().__init__()
-        self.upscale_factor = 2
-        self.up = nn.Upsample(scale_factor=2)
-        self.conv = nn.Conv2d(
-            ch_in, ch_out, kernel_size=3, stride=1, padding=1, bias=True
-        )
-        self.norm = LayerNorm(ch_out, eps=1e-6)
-        self.act = nn.GELU()
-
-    def forward(self, x):
-        x = self.up(x)
-        x = self.conv(x)
-        x = x.permute(0, 2, 3, 1)  # (N, C, H, W) -> (N, H, W, C)
-        x = self.norm(x)
-        x = x.permute(0, 3, 1, 2)  # (N, H, W, C) -> (N, C, H, W)
-        x = self.act(x)
-        return x
-
-
-class UpConvNext2(nn.Module):
-    def __init__(self, ch_in, ch_out):
-        super().__init__()
-        self.upscale_factor = 2
-        #self.pixel = nn.PixelShuffle(upscale_factor=self.upscale_factor)
-        self.up = nn.ConvTranspose2d(
-            ch_in ,
-            ch_in//2,
-            kernel_size=2,
-            stride=2,
-            padding=0,
-        )
-        #self.norm = LayerNorm(ch_out, eps=1e-6)
-        self.act = nn.GELU()
-
-    def forward(self, x):
-        #x = self.pixel(x)
-       
-        x = self.up(x)
-        x = x.permute(0, 2, 3, 1)  # (N, C, H, W) -> (N, H, W, C)
-        #x = self.norm(x)
-        x = x.permute(0, 3, 1, 2)  # (N, H, W, C) -> (N, C, H, W)
-        x = self.act(x)
-        return x
-
-
-class ConvNext_block(nn.Module):
-    def __init__(self, ch_in, ch_out):
-        super().__init__()
-        self.conv_next = nn.Sequential(ConvNectBlock(dim=ch_out))
-        self.Conv_1x1 = nn.Conv2d(ch_in, ch_out, kernel_size=1, stride=1, padding=0)
-
-    def forward(self, x):
-        x = self.Conv_1x1(x)
-        return self.conv_next(x)
+    def forward(self, x: Tensor) -> Dict[str, Tensor]:
+        out = OrderedDict()
+        for name, module in self.items():
+            x = module(x)
+            if name in self.return_layers:
+                out_name = self.return_layers[name]
+                out[out_name] = x
+        return out
 
 
 class U_ConvNext(nn.Module):
-    """
-    Version 1 using jast ConvNext_block. No bach normalization and no relu.
-    """
+    def __init__(self, num_classes, pretrain_backbone: bool = False):
+        super(U_ConvNext, self).__init__()
+        backbone = convnext_base(pretrained=pretrain_backbone)
 
-    def __init__(self, img_ch=3, num_classes: int = 2,channels:int=24):
-        super().__init__()
-        self.Maxpool = nn.MaxPool2d(kernel_size=2, stride=2)
-        self.dropout = nn.Dropout(0.125)
+        # if pretrain_backbone:
+        #     # 载入mobilenetv3 large backbone预训练权重
+        #     # https://download.pytorch.org/models/mobilenet_v3_large-8738ca79.pth
+        #     backbone.load_state_dict(torch.load("mobilenet_v3_large.pth", map_location='cpu'))
 
-        self.Conv1 = ConvNext_block(ch_in=img_ch, ch_out=channels)
-        self.Conv2 = ConvNext_block(ch_in=channels, ch_out=channels * 2)
-        self.Conv2x = ConvNext_block(ch_in=channels*2, ch_out=channels * 2)
-        self.Conv3 = ConvNext_block(ch_in=channels * 2, ch_out=channels * 4)
-        self.Conv3x = ConvNext_block(ch_in=channels * 4, ch_out=channels * 4)
-        self.Conv4 = ConvNext_block(ch_in=channels * 4, ch_out=channels * 8)
-        self.Conv4x = ConvNext_block(ch_in=channels * 8, ch_out=channels * 8)
-        self.Conv5 = ConvNext_block(ch_in=channels * 8, ch_out=channels * 16)
-        self.Conv5x = ConvNext_block(ch_in=channels * 16, ch_out=channels * 16)
+        backbone = backbone.features
+        stage_indices = [1, 3, 5, 7]
+        self.stage_out_channels = [128, 256, 512,1024]
+        return_layers = dict([(str(j), f"stage{i}") for i, j in enumerate(stage_indices)])
+        self.backbone = IntermediateLayerGetter(backbone, return_layers=return_layers)
 
-        self.Up5 = UpConvNext2(ch_in=channels * 16, ch_out=channels * 8)
-        self.Up_conv5 = ConvNext_block(ch_in=channels * 16, ch_out=channels * 8)
+        c = self.stage_out_channels[3] + self.stage_out_channels[2]
+        self.up1 = Up(c, self.stage_out_channels[2])
+        c = self.stage_out_channels[2] + self.stage_out_channels[1]
+        self.up2 = Up(c, self.stage_out_channels[1])
+        c = self.stage_out_channels[1] + self.stage_out_channels[0]
+        self.up3 = Up(c, self.stage_out_channels[0])
+        self.conv = OutConv(self.stage_out_channels[0], num_classes=num_classes)
 
-        self.Up4 = UpConvNext2(ch_in=channels * 8, ch_out=channels * 4)
-        self.Up_conv4 = ConvNext_block(ch_in=channels * 8, ch_out=channels * 4)
+    def forward(self, x: torch.Tensor) -> Dict[str, torch.Tensor]:
+        input_shape = x.shape[-2:]
+        backbone_out = self.backbone(x)
+        #print((backbone_out['stage3'].shape, backbone_out['stage2'].shape))
+        x = self.up1(backbone_out['stage3'], backbone_out['stage2'])
+        x = self.up2(x, backbone_out['stage1'])
+        x = self.up3(x, backbone_out['stage0'])
+        x = self.conv(x)
+        x = F.interpolate(x, size=input_shape, mode="bilinear", align_corners=False)
 
-        self.Up3 = UpConvNext2(ch_in=channels * 4, ch_out=channels * 2)
-        self.Up_conv3 = ConvNext_block(ch_in=channels * 4, ch_out=channels * 2)
-
-        self.Up2 = UpConvNext2(ch_in=channels * 2, ch_out=channels)
-        self.Up_conv2 = ConvNext_block(ch_in=channels * 2, ch_out=channels)
-
-        self.Conv_1x1 = nn.Conv2d(
-            channels, num_classes, kernel_size=1, stride=1, padding=0
-        )
-        self.last_activation = nn.Hardtanh()
-
-    def forward(self, x):
-        # encoding path
-        x1 = self.Conv1(x)
-        
-
-        x2 = self.Maxpool(x1)
-        x2 = self.Conv2(x2)
-        x2 = self.Conv2x(x2)
-        
-        x3 = self.Maxpool(x2)
-        x3 = self.Conv3(x3)
-        x3 = self.Conv3x(x3)
-       
-        x4 = self.Maxpool(x3)
-        x4 = self.Conv4(x4)
-        x4 = self.Conv4x(x4)
-        
-        x5 = self.Maxpool(x4)
-        x5 = self.Conv5(x5)
-        x5 = self.Conv5x(x5)
-              
-        # decoding + concat path
-       
-        d5 = self.Up5(x5)
-        d5 = torch.cat([d5,x4], dim=1)
-        d5 = self.Up_conv5(d5)
-
-        d4 = self.Up4(d5)
-        d4 = torch.cat((x3, d4), dim=1)
-        d4 = self.Up_conv4(d4)
-
-        d3 = self.Up3(d4)
-        d3 = torch.cat((x2, d3), dim=1)
-        d3 = self.Up_conv3(d3)
-
-        d2 = self.Up2(d3)
-        d2 = torch.cat((x1, d2), dim=1)
-        d2 = self.Up_conv2(d2)
-
-        d1 = self.Conv_1x1(d2)
-        d1 = self.last_activation(d1)
-
-        return {"out": d1}
+        return {"out": x}
