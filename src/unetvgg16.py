dee98a1303606354360190746a09d566f64557f5
Jacky-Android <55181594+Jacky-Android@users.noreply.github.com>
2022-12-17 13:43:35 +0800

    Update unetvgg16.py

--- src/unetvgg16.py
+++ src/unetvgg16.py
@@ -37,9 +37,9 @@ class Up(nn.Module):
             self.conv = DoubleConv(in_channels, out_channels)
 
     def forward(self, x1: torch.Tensor, x2: torch.Tensor) -> torch.Tensor:
-        print(x1.shape,x2.shape)
+        #print(x1.shape,x2.shape)
         x1 = self.up(x1)
-        print(x1.shape)
+        #print(x1.shape)
         # [N, C, H, W]
         diff_y = x2.size()[2] - x1.size()[2]
         diff_x = x2.size()[3] - x1.size()[3]
