a6a47662ce53dbf7402844b11ae4a8535f9b9166
Jacky-Android <55181594+Jacky-Android@users.noreply.github.com>
2022-12-17 15:17:44 +0800

    Update convnext_unet.py

--- src/convnext_unet.py
+++ src/convnext_unet.py
@@ -9,7 +9,25 @@ from .unetvgg16 import Up, OutConv
 
 
 class IntermediateLayerGetter(nn.ModuleDict):
-    
+    """
+    Module wrapper that returns intermediate layers from a model
+
+    It has a strong assumption that the modules have been registered
+    into the model in the same order as they are used.
+    This means that one should **not** reuse the same nn.Module
+    twice in the forward if you want this to work.
+
+    Additionally, it is only able to query submodules that are directly
+    assigned to the model. So if `model` is passed, `model.feature1` can
+    be returned, but not `model.feature1.layer2`.
+
+    Args:
+        model (nn.Module): model on which we will extract the features
+        return_layers (Dict[name, new_name]): a dict containing the names
+            of the modules for which the activations will be returned as
+            the key of the dict, and the value of the dict is the name
+            of the returned activation (which the user can specify).
+    """
     _version = 2
     __annotations__ = {
         "return_layers": Dict[str, str],
@@ -54,27 +72,30 @@ class U_ConvNext(nn.Module):
         #     backbone.load_state_dict(torch.load("mobilenet_v3_large.pth", map_location='cpu'))
 
         backbone = backbone.features
-        stage_indices = [1, 3, 5, 7]
-        self.stage_out_channels = [128, 256, 512,1024]
+        
+        stage_indices = [0,1,2,3,4,5,6,7]
+        self.stage_out_channels = [128, 128,256,256,512, 512,1024,1024]
         return_layers = dict([(str(j), f"stage{i}") for i, j in enumerate(stage_indices)])
         self.backbone = IntermediateLayerGetter(backbone, return_layers=return_layers)
 
+        c = self.stage_out_channels[4] + self.stage_out_channels[3]
+        self.up1 = Up(c, self.stage_out_channels[3])
         c = self.stage_out_channels[3] + self.stage_out_channels[2]
-        self.up1 = Up(c, self.stage_out_channels[2])
+        self.up2 = Up(c, self.stage_out_channels[2])
         c = self.stage_out_channels[2] + self.stage_out_channels[1]
-        self.up2 = Up(c, self.stage_out_channels[1])
+        self.up3 = Up(c, self.stage_out_channels[1])
         c = self.stage_out_channels[1] + self.stage_out_channels[0]
-        self.up3 = Up(c, self.stage_out_channels[0])
+        self.up4 = Up(c, self.stage_out_channels[0])
         self.conv = OutConv(self.stage_out_channels[0], num_classes=num_classes)
 
     def forward(self, x: torch.Tensor) -> Dict[str, torch.Tensor]:
         input_shape = x.shape[-2:]
         backbone_out = self.backbone(x)
-        #print((backbone_out['stage3'].shape, backbone_out['stage2'].shape))
-        x = self.up1(backbone_out['stage3'], backbone_out['stage2'])
-        x = self.up2(x, backbone_out['stage1'])
-        x = self.up3(x, backbone_out['stage0'])
+        
+        x = self.up1(backbone_out['stage4'], backbone_out['stage3'])
+        x = self.up2(x, backbone_out['stage2'])
+        x = self.up3(x, backbone_out['stage1'])
+        x = self.up4(x, backbone_out['stage0'])
         x = self.conv(x)
-        x = F.interpolate(x, size=input_shape, mode="bilinear", align_corners=False)
-
+        
         return {"out": x}
