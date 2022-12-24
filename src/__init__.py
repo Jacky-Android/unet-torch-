8d458df4600c08d51d31c4d4d8844b1c857b06aa
Jacky-Android <55181594+Jacky-Android@users.noreply.github.com>
2022-12-24 16:15:53 +0800

    Update __init__.py

--- src/__init__.py
+++ src/__init__.py
@@ -1,4 +1,6 @@
 from .unet import UNet
 from .mobilenet_unet import MobileV3Unet
 from .vgg_unet import VGG16UNet
+from .unetvgg16 import UNet
 from .convnext_unet import U_ConvNext
+from .unet_convnext import Unet_ConvNext
