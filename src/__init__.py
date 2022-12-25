e864808c0a9f0de6b2e1124ff37910bdac647d40
Jacky-Android <55181594+Jacky-Android@users.noreply.github.com>
2022-12-25 11:06:59 +0800

    Update __init__.py

--- src/__init__.py
+++ src/__init__.py
@@ -4,4 +4,4 @@ from .vgg_unet import VGG16UNet
 from .unetvgg16 import UNet
 from .convnext_unet import U_ConvNext
 from .unet_convnext import Unet_ConvNext
-from .unet_networks import U_Net,AttU_Net,R2U_Net
+from .unet_networks import U_Net,AttU_Net,R2U_Net,R2AttU_Net
