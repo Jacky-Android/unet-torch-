77ef45dc968165254a3c7a6dd88531a9a1d7923c
Jacky-Android <55181594+Jacky-Android@users.noreply.github.com>
2023-01-07 10:59:15 +0800

    Update unet_test.py

--- src/unet_test.py
+++ src/unet_test.py
@@ -3,38 +3,35 @@ import torch
 import torch.nn as nn
 import torch.nn.functional as F
 
-class SELayer(nn.Module):
-    def __init__(self, channel, reduction=16):
-        super(SELayer, self).__init__()
-        self.avg_pool = nn.AdaptiveAvgPool2d(1)
-        self.fc = nn.Sequential(
-            nn.Linear(channel, channel // reduction, bias=False),
-            nn.ReLU(inplace=True),
-            nn.Linear(channel // reduction, channel, bias=False),
+class Attention_block(nn.Module):
+    def __init__(self,F_g,F_l,F_int):
+        super(Attention_block,self).__init__()
+        self.W_g = nn.Sequential(
+            nn.Conv2d(F_g, F_int, kernel_size=1,stride=1,padding=0,bias=True),
+            nn.BatchNorm2d(F_int)
+            )
+        
+        self.W_x = nn.Sequential(
+            nn.Conv2d(F_l, F_int, kernel_size=1,stride=1,padding=0,bias=True),
+            nn.BatchNorm2d(F_int)
+        )
+
+        self.psi = nn.Sequential(
+            nn.Conv2d(F_int, 1, kernel_size=1,stride=1,padding=0,bias=True),
+            nn.BatchNorm2d(1),
             nn.Sigmoid()
         )
+        
+        self.relu = nn.ReLU(inplace=True)
+        
+    def forward(self,g,x):
+        g1 = self.W_g(g)
+        x1 = self.W_x(x)
+        psi = self.relu(g1+x1)
+        psi = self.psi(psi)
 
-    def forward(self, x):
-        b, c, _, _ = x.size()
-        y = self.avg_pool(x).view(b, c)
-        y = self.fc(y).view(b, c, 1, 1)
-        out = x * y.expand_as(x)
-        return out
-class conv_block(nn.Module):
-    def __init__(self,ch_in,ch_out):
-        super(conv_block,self).__init__()
-        self.conv = nn.Sequential(
-            nn.Conv2d(ch_in, ch_out, kernel_size=3,stride=1,padding=1,bias=True),
-            nn.BatchNorm2d(ch_out),
-            nn.ReLU(inplace=True),
-            nn.Conv2d(ch_out, ch_out, kernel_size=3,stride=1,padding=1,bias=True),
-            nn.BatchNorm2d(ch_out),
-            nn.ReLU(inplace=True)
-        )
+        return x*psi
 
-    def forward(self,x):
-        x = self.conv(x)
-        return x
 def drop_path(x, drop_prob: float = 0., training: bool = False):
     """Drop paths (Stochastic Depth) per sample (when applied in main path of residual blocks).
     This is the same as the DropConnect impl I created for EfficientNet, etc networks, however,
@@ -158,6 +155,9 @@ class up_conv(nn.Module):
             nn.Upsample(scale_factor=2),
             nn.Conv2d(ch_in,ch_out,kernel_size=3,stride=1,padding=1,bias=True),
 		    nn.BatchNorm2d(ch_out),
+			nn.ReLU(inplace=True),
+            nn.Conv2d(ch_out,ch_out,kernel_size=3,stride=1,padding=1,bias=True),
+		    nn.BatchNorm2d(ch_out),
 			nn.ReLU(inplace=True)
         )
 
@@ -165,55 +165,51 @@ class up_conv(nn.Module):
         x = self.up(x)
         return x
 
-class U_Net_convnextblock(nn.Module):   
+class convnextAttU_Net(nn.Module):   
     def __init__(self,img_ch=3,num_classes:int=2,layer_scale_init_value:float=1e-6):
-        super(U_Net_convnextblock,self).__init__()
+        super(convnextAttU_Net,self).__init__()
         self.relu=nn.ReLU()
         #self.Maxpool = nn.MaxPool2d(kernel_size=2,stride=2)
         #self.skipconv = nn.conv2d(64,64, kernel_size=1, stride=2)
        
         self.Conv1 = conv_block(ch_in=img_ch,ch_out=64) #64
-        self.se1 = SELayer(64)
         self.block1 = Block(dim=64,layer_scale_init_value=layer_scale_init_value)
-        self.skipconv1 = nn.Conv2d(64,64, kernel_size=2, stride=2)
         self.downlayers1 = nn.Conv2d(64,64, kernel_size=2, stride=2)
         self.layernorm1 = LayerNorm(64, eps=1e-6, data_format="channels_first")
 
         self.Conv2 = conv_block(ch_in=64,ch_out=128)  #64 128
         self.block2 = Block(dim=128,layer_scale_init_value=layer_scale_init_value)
-        self.se2 = SELayer(128)
-        self.skipconv2 = nn.Conv2d(128,128, kernel_size=1, stride=2)
         self.downlayers2 = nn.Conv2d(128,128, kernel_size=2, stride=2)
         self.layernorm2 = LayerNorm(128, eps=1e-6, data_format="channels_first")
         
         self.Conv3 = conv_block(ch_in=128,ch_out=256) #128 256
         self.block3 = Block(dim=256,layer_scale_init_value=layer_scale_init_value)
-        self.se3 = SELayer(256)
-        self.skipconv3 = nn.Conv2d(256,256, kernel_size=1, stride=2)
         self.downlayers3 = nn.Conv2d(256,256, kernel_size=2, stride=2)
         self.layernorm3 = LayerNorm(256, eps=1e-6, data_format="channels_first")
 
         self.Conv4 = conv_block(ch_in=256,ch_out=512) #256 512
         self.block4 = Block(dim=512,layer_scale_init_value=layer_scale_init_value)
-        self.se4 = SELayer(512)
-        self.skipconv4 = nn.Conv2d(512,512, kernel_size=1, stride=2)
         self.downlayers4 = nn.Conv2d(512,512, kernel_size=2, stride=2)
         self.layernorm4 = LayerNorm(512, eps=1e-6, data_format="channels_first")
 
         self.Conv5 = conv_block(ch_in=512,ch_out=1024) #512 1024
 
         
-        self.Up5 = up_conv(ch_in=1024,ch_out=512)  #1024 512
-        self.Up_conv5 = conv_block(ch_in=1024, ch_out=512)  
+        self.Up5 = up_conv(ch_in=1024,ch_out=512)
+        self.Att5 = Attention_block(F_g=512,F_l=512,F_int=256)
+        self.Up_conv5 = conv_block(ch_in=1024, ch_out=512)
 
-        self.Up4 = up_conv(ch_in=512,ch_out=256)  #512 256
-        self.Up_conv4 = conv_block(ch_in=512, ch_out=256)  
+        self.Up4 = up_conv(ch_in=512,ch_out=256)
+        self.Att4 = Attention_block(F_g=256,F_l=256,F_int=128)
+        self.Up_conv4 = conv_block(ch_in=512, ch_out=256)
         
-        self.Up3 = up_conv(ch_in=256,ch_out=128)  #256 128
-        self.Up_conv3 = conv_block(ch_in=256, ch_out=128) 
+        self.Up3 = up_conv(ch_in=256,ch_out=128)
+        self.Att3 = Attention_block(F_g=128,F_l=128,F_int=64)
+        self.Up_conv3 = conv_block(ch_in=256, ch_out=128)
         
-        self.Up2 = up_conv(ch_in=128,ch_out=64) #128 64
-        self.Up_conv2 = conv_block(ch_in=128, ch_out=64)  
+        self.Up2 = up_conv(ch_in=128,ch_out=64)
+        self.Att2 = Attention_block(F_g=64,F_l=64,F_int=32)
+        self.Up_conv2 = conv_block(ch_in=128, ch_out=64) 
 
         self.Conv_1x1 = nn.Conv2d(64,num_classes,kernel_size=1,stride=1,padding=0)  #64
 
@@ -224,50 +220,48 @@ class U_Net_convnextblock(nn.Module):
         x1 = self.Conv1(x)
         x1 = self.block1(self.block1(x1))
 
-        x2 = self.skipconv1(x1)+self.layernorm1(self.downlayers1(x1))
-        x2 = self.se1(x2)
+        x2 = self.layernorm1(self.downlayers1(x1))
         x2 = self.Conv2(x2)
         x2 = self.block2(self.block2(x2))
 
-        x3 = self.skipconv2(x2)+self.layernorm2(self.downlayers2(x2))
-        x3 = self.se2(x3)
+        x3 = self.layernorm2(self.downlayers2(x2))
         x3 = self.Conv3(x3)
         x3 = self.block3(self.block3(x3))
         
 
-        x4 = self.skipconv3(x3)+self.layernorm3(self.downlayers3(x3))
-        x4 = self.se3(x4)
+        x4 = self.layernorm3(self.downlayers3(x3))
         x4 = self.Conv4(x4)
         x4 = self.block4(self.block4(x4))
 
-        x5 = self.skipconv4(x4)+self.layernorm4(self.downlayers4(x4))
+        x5 = self.layernorm4(self.downlayers4(x4))
         x5 = self.Conv5(x5)
 
         # decoding + concat path
         d5 = self.Up5(x5)
+        x4 = self.Att5(g=d5,x=x4)
         d5 = torch.cat((x4,d5),dim=1)
         #d5 = self.relu(d5+x4)
         #print(d5.shape)
         d5 = self.Up_conv5(d5)
-        d5 = self.block4(d5)
         
         d4 = self.Up4(d5)
+        x3 = self.Att4(g=d4,x=x3)
         d4 = torch.cat((x3,d4),dim=1)
         #d4 = self.relu(d4+x3)
         d4 = self.Up_conv4(d4)
-        d4 = self.block3(d4)
 
         d3 = self.Up3(d4)
+        x2 = self.Att3(g=d3,x=x2)
         d3 = torch.cat((x2,d3),dim=1)
         #d3 = self.relu(d3+x2)
         d3 = self.Up_conv3(d3)
-        d3 = self.block2(d3)
 
         d2 = self.Up2(d3)
+        x1 = self.Att2(g=d2,x=x1)
         d2 = torch.cat((x1,d2),dim=1)
         #d2 = self.relu(d2+x1)
         d2 = self.Up_conv2(d2)
-        d2 = self.block1(d2)
+       
 
         d1 = self.Conv_1x1(d2)
 
